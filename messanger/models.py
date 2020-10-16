4c4600043f900a3728ee213dbd8aaa1fa20054ad
desertsniper87 <torshobuet@gmail.com>
2020-10-16 17:22:33 +0600

    implement crypto

--- messanger/models.py
+++ messanger/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django_cryptography.fields import encrypt
 
 
 class Message(models.Model):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
-    content = models.TextField()
+    content = encrypt(models.TextField())
